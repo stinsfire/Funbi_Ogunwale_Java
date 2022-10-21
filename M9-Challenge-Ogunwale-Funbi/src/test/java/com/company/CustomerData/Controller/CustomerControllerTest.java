86f409051bf75a44cf20076279906d6ea6f2925e
stinsfire <stinsfire007@gmail.com>
2022-10-20 21:11:22 -0400

    module 9 challenge solution
    Source files for module 9 challenge

---
+++ M9-Challenge-Ogunwale-Funbi/src/test/java/com/company/CustomerData/Controller/CustomerControllerTest.java
@@ -0,0 +1,26 @@
+package com.company.CustomerData.Controller;
+
+import com.company.CustomerData.Repository.CustomerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+
+}
