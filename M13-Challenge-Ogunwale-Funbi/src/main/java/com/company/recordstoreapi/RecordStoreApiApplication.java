41a331c7d54289e84741085b43991b4b17b08f19
stinsfire <stinsfire007@gmail.com>
2022-11-17 19:44:25 -0500

    m13 challenge
    m13 challenge source files

---
+++ M13-Challenge-Ogunwale-Funbi/src/main/java/com/company/recordstoreapi/RecordStoreApiApplication.java
@@ -0,0 +1,14 @@
+package com.company.recordstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class RecordStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordStoreApiApplication.class, args);
+	}
+
+}
